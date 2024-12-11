efce393324040106891159f44931a94ba3db7d40
irhndt 4fk <pro.irhndt@4fk.fr>
2024-12-11 21:31:38 +0000

    adding description and display for address set
    Signed-off-by: irhndt 4fk <pro.irhndt@4fk.fr>

--- shared/api/network_addres_set.go
+++ shared/api/network_addres_set.go
@@ -20,6 +20,10 @@ type NetworkAddressSet struct {
 	// Mapping of key-value pairs for custom use
 	// Example: {"prod": "false"}
 	ExternalIDs map[string]string `json:"external_ids,omitempty" yaml:"external_ids,omitempty"`
+
+	// Description of the address set
+	// Example: Server storing secrets
+	Description string `json:"description,omitempty" yaml:"description,omitempty"`
 }
 
 // NetworkAddressSetPost used for renaming an Address Set.
@@ -44,6 +48,10 @@ type NetworkAddressSetPut struct {
 
 	// Updated external_ids
 	ExternalIDs map[string]string `json:"external_ids,omitempty" yaml:"external_ids,omitempty"`
+
+	// Description of the ACL
+	// Example: Web servers
+	Description string `json:"description" yaml:"description"`
 }
 
 // NetworkAddressSetsPost used for creating a new Address Set.
@@ -54,6 +62,27 @@ type NetworkAddressSetPut struct {
 type NetworkAddressSetsPost struct {
 	NetworkAddressSetPut  `yaml:",inline"`
 	NetworkAddressSetPost `yaml:",inline"`
+}
+
+// NetworkAddressSetView used for displaying an addres set.
+//
+// swagger:model
+//
+// API extension: network_address_set.
+type NetworkAddressSetView struct {
+	NetworkAddressSetPut  `yaml:",inline"`
+	NetworkAddressSetPost `yaml:",inline"`
+
+	// List of URLs of objects using this profile
+	// Read only: true
+	// Example: ["/1.0/instances/c1", "/1.0/instances/v1", "/1.0/networks/mybr0"]
+	UsedBy []string `json:"used_by" yaml:"used_by"` // Resources that use the AddressSet.
+
+	// Project name
+	// Example: project1
+	//
+	// API extension: network_address_set_all_projects
+	Project string `json:"project" yaml:"project"` // Project the AddressSet belongs to.
 }
 
 // Normalise normalises fields in the NetworkAddressSet so that comparisons are consistent.
