2abe7fc6ff142ea01b5d1e1b57c20a9b07e92052
Isidore Reinhardt <pro.irhndt@4fk.fr>
2025-03-02 22:17:12 +0100

    reformatting
    Signed-off-by: Isidore Reinhardt <pro.irhndt@4fk.fr>

--- internal/version/api.go
+++ internal/version/api.go
@@ -465,7 +465,7 @@ var APIExtensions = []string{
 	"instance_smbios11",
 	"api_filtering_extended",
 	"acme_dns01",
-        "network_address_set",
+	"network_address_set",
 }
 
 // APIExtensionsCount returns the number of available API extensions.
